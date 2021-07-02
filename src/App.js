902318a0e4e663fa6ab25eb3ff3b406a73861854
Pravin <khabilepravin@gmail.com>
2021-07-03 09:11:51 +1000

    Introduced route switch

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpellCheck } from "@fortawesome/free-solid-svg-icons";
 import { Col, Row } from "reactstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import SpellTest from "./components/spelltest";
+import Result from "./components/result";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Row>
-          <Col>
-            <FontAwesomeIcon icon={faSpellCheck} /> Spell
-          </Col>
-        </Row>
-      </header>
-
-      <SpellTest />
-    </div>
+    <Router>
+      {" "}
+      <div className="App">
+        <header className="App-header">
+          <Row>
+            <Col>
+              <FontAwesomeIcon icon={faSpellCheck} /> Spell
+            </Col>
+          </Row>
+        </header>
+        <Switch>
+          <Route path="/" exact component={SpellTest} />
+          <Route path="/result" component={Result} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
