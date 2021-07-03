bcc37e402d7ed53e9db0d9de92cb6d5d92422055
Pravin <khabilepravin@gmail.com>
2021-07-04 05:14:22 +1000

    Added form to make the answer submission on ENTER

--- src/components/spelltest.js
+++ src/components/spelltest.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useHistory } from "react-router-dom";
 
-import { Button, Container, Col, Row, Progress, Input } from "reactstrap";
+import { Button, Container, Col, Row, Progress, Input, Form } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faForward, faCheck } from "@fortawesome/free-solid-svg-icons";
 import Word from "./word";
@@ -10,14 +10,13 @@ import { useGetData } from "../useGetDataHook";
 
 export const verifyIfCorrect = (word, userAnswer) => {
   if (word && userAnswer) {
-    if (word.toUpperCase() == userAnswer.toUpperCase()) {
+    if (word.trim().toUpperCase() == userAnswer.trim().toUpperCase()) {
       return true;
     } else {
       return false;
     }
-  }
-  else{
-      return false;
+  } else {
+    return false;
   }
 };
 
@@ -30,79 +29,100 @@ const SpellTest = () => {
   const [buttonClass, setButtonClass] = useState("primary");
   const [currentIndex, setCurrentIndex] = useState(0);
   const [results, setResults] = useState([]);
-  const [userAnswer, setUserAnswer] = useState('');
-  
-  const moveToNextWord = () => {
-    setUserAnswer('');
-    let isCorrect = verifyIfCorrect(data[currentIndex].wordData.word, userAnswer);
-    setResults(results => [...results, { word: data[currentIndex].wordData.word, userEnteredAnswer: userAnswer, isCorrect: isCorrect }]);
+  const [userAnswer, setUserAnswer] = useState("");
+
+  const moveToNextWord = (event) => {
+    event.preventDefault();
+    setUserAnswer("");
+    let isCorrect = verifyIfCorrect(
+      data[currentIndex].wordData.word,
+      userAnswer
+    );
+    setResults((results) => [
+      ...results,
+      {
+        word: data[currentIndex].wordData.word,
+        userEnteredAnswer: userAnswer,
+        isCorrect: isCorrect,
+      },
+    ]);
 
     setCurrentIndex(currentIndex + 1);
 
-    if(currentIndex === totalWordsInATest -2) {
+    if (currentIndex === totalWordsInATest - 2) {
       setButtonText("Done");
       setButtonClass("success");
     }
 
-     if (currentIndex === totalWordsInATest - 1) {
-       let finalResult = results.slice();
-       // Push the last item before redirecting
-      let isCorrect = verifyIfCorrect(data[currentIndex].wordData.word, userAnswer); 
-      finalResult.push({ word: data[currentIndex].wordData.word, userEnteredAnswer: userAnswer, isCorrect: isCorrect });
+    if (currentIndex === totalWordsInATest - 1) {
+      let finalResult = results.slice();
+      // Push the last item before redirecting
+      let isCorrect = verifyIfCorrect(
+        data[currentIndex].wordData.word,
+        userAnswer
+      );
+      finalResult.push({
+        word: data[currentIndex].wordData.word,
+        userEnteredAnswer: userAnswer,
+        isCorrect: isCorrect,
+      });
       history.push("/result", finalResult);
-     }
+    }
   };
 
-  const handleUserAnswer = (event) =>{
+  const handleUserAnswer = (event) => {
     setUserAnswer(event.target.value);
   };
 
   if (data.length > 0) {
     return (
-      <Container>
-        <Row>
-          <Col>
-          <br/>
-            <Word currentWord={data[currentIndex].wordData} />
-            <br />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Input
-              type="text"
-              placeholder="Enter Spelling"
-              spellCheck="false"
-              autoComplete="false"
-              autoCorrect="false"
-              autoCapitalize="false"
-              className="input-lg"
-              ref={userAnswerRef}
-              value={userAnswer}
-              onChange={handleUserAnswer}
-            />
-            <br />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Progress value={currentIndex + 1} max={totalWordsInATest}>
-              {currentIndex + 1} of {totalWordsInATest}
-            </Progress>
-            <br />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button color={buttonClass} onClick={moveToNextWord}>
-              <FontAwesomeIcon
-                icon={buttonText == "Done" ? faCheck : faForward}
-              />{" "}
-              {buttonText}
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+      <Form onSubmit={moveToNextWord}>
+        <Container>
+          <Row>
+            <Col>
+              <br />
+              <Word currentWord={data[currentIndex].wordData} />
+              <br />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Input
+                type="text"
+                placeholder="Enter Spelling"
+                spellCheck="false"
+                autoComplete="false"
+                autoCorrect="false"
+                autoCapitalize="false"
+                autoFocus="true"
+                className="input-lg"
+                ref={userAnswerRef}
+                value={userAnswer}
+                onChange={handleUserAnswer}
+              />
+              <br />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Progress value={currentIndex + 1} max={totalWordsInATest}>
+                {currentIndex + 1} of {totalWordsInATest}
+              </Progress>
+              <br />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button color={buttonClass} type="submit">
+                <FontAwesomeIcon
+                  icon={buttonText == "Done" ? faCheck : faForward}
+                />{" "}
+                {buttonText}
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </Form>
     );
   } else {
     return <h3>Loading...</h3>;
